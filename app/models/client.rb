f5d3976be6503b23e02976906f33e749b855c9e6
Hector Alpuche Rodriguez <hector.alprod.2021@gmail.com>
2023-01-18 10:52:19 -0500

    feat: :poop: se agrego la tabla de pagos dentro del show de contrato, entre otras cosillas

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,4 +8,7 @@ class Client < ApplicationRecord
   has_many :contracts
 
   validates :first_name, :middle_name, :last_name, presence: true
+
+  scope :active, -> { where("status=true") }
+  scope :order_by_name, -> { order(first_name: :asc) }
 end
